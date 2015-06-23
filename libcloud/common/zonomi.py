e070f54350a9dfa079f503775baafcf2f916f6ae
oltjano <oltjano13@gmail.com>
2015-06-23 19:06:44 +0200

    Working on Zonomi base classes

--- libcloud/common/zonomi.py
+++ libcloud/common/zonomi.py
@@ -1,11 +1,16 @@
 from libcloud.common.base import XmlResponse
 from libcloud.common.base import ConnectionKey
 
+#Endpoint for Zonomi API.
+API_HOST = 'https://zonomi.com/app/dns/dyndns.jsp?'
+
 class ZonomiResponse(XmlResponse):
     pass
 
 
 class ZonomiConnection(ConnectionKey):
+    host = API_HOST
+    responseCls = ZonomiResponse
 
     def add_default_params(self, params):
         """
@@ -15,3 +20,10 @@ class ZonomiConnection(ConnectionKey):
         params['api_key'] = self.key
 
         return params
+
+    def add_default_headers(self, headers):
+        """
+        Adds default headers needed to perform a successful
+        request such as Content-Type, User-Agent.
+        """
+        return headers
